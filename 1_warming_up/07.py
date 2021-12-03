c856e03071a45d004bab5857bf6ae53c425b375d
miya-38-a <85498974+miya-38-a@users.noreply.github.com>
2021-12-04 01:27:57 +0900

    add 07.py

--- 1_warming_up/07.py
+++ 1_warming_up/07.py
@@ -1,12 +1,30 @@
 #集合
 
-def template(x, y, z):
-    str1 = str(x) + "時の" + str(y) + "は" + str(z)
-    
-    return str1
+def cipher(target):
+    list_target = list(target)
+
+    #普通に書く
+    """
+    list_crypt = []
+    for i in range(len(list_target)):
+        if list_target[i].islower():
+            list_crypt.append(chr(219-ord(list_target[i])))
+        else:
+            list_crypt.append(list_target[i])
+    """
+
+    #1行で書く
+    #list_crypt = [chr(219-ord(list_target[i])) if list_target[i].islower() else list_target[i] for i in range (len(list_target))]
+
+    #さらに短く
+    list_crypt = [chr(219-ord(x)) if x.islower() else x for x in list_target]
+
+    str_crypt = "".join(list_crypt)
+
+    return str_crypt
 
 def main():
-    ans = template(12,"気温", 22.4)
+    ans = cipher("Hello World!")
 
     print(ans)
 
