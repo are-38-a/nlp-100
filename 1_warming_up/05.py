b5d6a565f06b02d108aa2677e4d15dcef1ae2131
miya-38-a <85498974+miya-38-a@users.noreply.github.com>
2021-12-03 22:55:56 +0900

    add 05.py

---
+++ 1_warming_up/05.py
@@ -0,0 +1,21 @@
+#n-gram
+
+def n_gram(n, list_target):
+    list_n_gram = []
+
+    for i in range(len(list_target) - n + 1):
+        list_n_gram.append(" ".join(list_target[i:i+n]))
+
+    return list_n_gram
+
+def main():
+    str1 = "I am an NLPer"
+
+    word_bi_gram = n_gram(2,str1.split())
+    char_bi_gram = n_gram(2,list(str1))
+    
+    print(word_bi_gram)
+    print(char_bi_gram)
+
+if __name__=="__main__":
+    main()
